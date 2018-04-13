8c5247a97b185ce556cc8d608edfd85dd10aa045
Razzfield <Razzfield@gmail.com>
2018-04-13 23:59:11 +0700

    database

--- config/database.php
+++ config/database.php
@@ -1,4 +1,9 @@
 <?php
+  $url = parse_url(getenv("DATABASE_URL"));
+  $host = $url["host"]??null;
+  $username = $url["user"]??null;
+  $password = $url["pass"]??null;
+  $database = substr($url["path"],1)??null;
 
 return [
 
@@ -13,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,15 +61,13 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
